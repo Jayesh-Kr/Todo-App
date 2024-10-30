09a80fe70b40ba1246eebd952c0a07995c138587
Jayesh Krishna <121498525+Jayesh-Kr@users.noreply.github.com>
2024-10-30 10:31:51 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 (async()=>{
-   await mongoose.connect(process.env.MONGO_URI);
+   await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to mongoDB");
 })();
 
